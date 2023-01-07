20af3210096e8af1c8971f956a5cf8a61fceb952
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-01-07 13:35:29 +0530

    Factorial_Trailing Zeros

---
+++ Goldman Sachs/Factorial_Trailing_Zeroes.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) 
+    {
+        int count=0;
+        for(int i=5;i<=n;i=i*5)
+        {
+            count=count+n/i;
+        }
+        return count;
+    }
+};
